9ce7f4914ba6aa26ac377fd2f1f18c7cc14d6939
Charles Crabtree <crabtreedcharles@gmail.com>
2017-02-04 01:26:15 -0500

    enable continuous integration via craigcitro/r-travis

--- R/bivarplots.r
+++ R/bivarplots.r
@@ -4,7 +4,7 @@ bivarplots <- function(x, y, unit, plotdata) {
   with(plotdata, {
   for(i in 1:length(unique(plotdata$unit))) {
     j <- unique(plotdata$unit)[i]
-    bivar.j <- ggplot2::ggplot(plotdata[plotdata$unit==j,], aes_string(x = 'x', y = 'y')) + ggplot2::geom_jitter() + ggplot2::geom_smooth(method=loess) + ggplot2::labs(title=j)
+    bivar.j <- ggplot2::ggplot(plotdata[plotdata$unit==j,], aes_string(x = 'x', y = 'y')) + ggplot2::geom_jitter() + ggplot2::geom_smooth(method = loess) + ggplot2::labs(title = j)
     bivar.j <- bivar.j + ggplot2::theme_bw()
     print(bivar.j)
   }
---
+++ R/violinplots.r
@@ -0,0 +1,13 @@
+#--- Violin plots function ---#
+
+violinplots <- function(x, y, unit, plotdata) {
+  with(plotdata, {
+    for(i in 1:length(unique(plotdata$unit))) {
+      j <- unique(plotdata$unit)[i]
+      violin.j <- ggplot2::ggplot(plotdata[plotdata$unit==j, ], aes_string(x = 'x', y = 'y')) + ggplot2::geom_violin() + ggplot2::labs(title = j)
+      violin.j <- violin.j + ggplot2::theme_bw()
+      print(violin.j)
+    }
+  }
+  )
+}
