8589078fdae00184be0717fc4fa0278c8a247581
cdcrabtree <crabtreedcharles@gmail.com>
2017-02-09 17:03:07 -0500

    update groupcor

--- R/groupcor.r
+++ R/groupcor.r
@@ -2,8 +2,8 @@
 #' @export
 
 groupcor <- function(x, y, unit, plotdata, stat) {
- with(plotdata, {dat dplyr::%>%
-    dplyr::group_by(plotdata$unit) dplyr::%>%
+ with(plotdata, {dat %>%
+    dplyr::group_by(plotdata$unit) %>%
     dplyr::summarise(correlation == cor(x, y, na.rm = TRUE, method = c("stat")))
     print(dat)
  }
